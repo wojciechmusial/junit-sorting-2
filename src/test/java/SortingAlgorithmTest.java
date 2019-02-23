7c36fd4920cfb0bc53a6099d459327c2c9f348c7
AsiaJoanna <joannajablonka@gmail.com>
2019-02-23 12:11:44 +0100

    testy

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -34,4 +34,24 @@ public class SortingAlgorithmTest {
 
     }
 
+    private SortingAlgorithms classUnderTest;
+
+    @Before
+    public void setUp() throws Exception {
+        classUnderTest = new SortingAlgorithms();
+    }
+
+    @Test
+    public void testSortowaniaJJ1() {
+
+        double[] toSort = {5, 1, 3, 2};
+        double[] expected = {1, 2, 3, 5};
+
+        classUnderTest.sort (toSort, true);
+        Assert.assertArrayEquals (expected, toSort, 0.001);
+
+        }
+
+    }
+
 }
